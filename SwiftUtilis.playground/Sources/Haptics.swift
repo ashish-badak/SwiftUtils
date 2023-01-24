2869708b840c983889a83ffdca11d9557b331d81
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-01-25 02:17:44 +0530

    created Haptics struct to provide haptic feedback

---
+++ SwiftUtilis.playground/Sources/Haptics.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+struct Haptics {
+    /// This function provides notification haptics as `error`, `warning`, `success`
+    /// - Parameter notification: `UINotificationFeedbackGenerator.FeedbackType` case value
+    static func notify(_ notification: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.prepare()
+        generator.notificationOccurred(notification)
+    }
+    
+    /// This function provides haptic feedback of impact style -> `light`, `medium`, `heavy`
+    /// - Parameter impact: `UIImpactFeedbackGenerator.FeedbackStyle` case value
+    static func markImpact(intensity: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: intensity)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+    
+    /// This function provides haptic feedback of item selection just like UIPicker.
+    static func selectionEffect() {
+        let generator = UISelectionFeedbackGenerator()
+        generator.prepare()
+        generator.selectionChanged()
+    }
+}

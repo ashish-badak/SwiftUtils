51e47e19fe8d5aed53187d4ff02185472e4f8545
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-01-25 17:00:07 +0530

    created fading view

---
+++ SwiftUtilis.playground/Sources/FadingView.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+protocol FadingView {
+    func fadeIn(
+        duration: TimeInterval,
+        delay: TimeInterval,
+        completion: ((Bool) -> Void)?
+    )
+    func fadeOut(
+        duration: TimeInterval,
+        delay: TimeInterval,
+        completion: ((Bool) -> Void)?
+    )
+}
+
+extension FadingView where Self: UIView {
+    func fadeIn(
+        duration: TimeInterval = 0.5,
+        delay: TimeInterval = 0.0,
+        completion: ((Bool) -> Void)? = nil
+    ) {
+        alpha = 0.0
+
+        UIView.animate(
+            withDuration: duration,
+            delay: delay,
+            options: .curveEaseIn,
+            animations: { [weak self] in
+                self?.isHidden = false
+                self?.alpha = 1.0
+            },
+            completion: completion
+        )
+    }
+    
+    func fadeOut(
+        duration: TimeInterval = 0.5,
+        delay: TimeInterval = 0.0,
+        completion: ((Bool) -> Void)? = nil
+    ) {
+        alpha = 1.0
+
+        UIView.animate(
+            withDuration: duration,
+            delay: delay,
+            options: .curveEaseIn,
+            animations: { [weak self] in
+                self?.alpha = 0.0
+            }
+        ) { [weak self] (completed) in
+            self?.isHidden = true
+            completion?(true)
+        }
+    }
+}
